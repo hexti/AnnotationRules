1ec4a82c2b47341486a90534c915f6606a8eb5d7
hexti <anderson.bask@gmail.com>
2018-06-20 18:32:08 -0300

    Trocado o local que chama as classes do dump e degug.Closes #10.

--- web/index.php
+++ web/index.php
@@ -1,11 +1,6 @@
 <?php
+require __DIR__ . '/../vendor/autoload.php';
 include_once '../app/config/Annotation.class.php';
 include_once '../app/Controller/MyClass.class.php';
-use Symfony\Component\Debug\Debug;
-use Symfony\Component\Debug\ErrorHandler;
-use Symfony\Component\Debug\ExceptionHandler;
-use Symfony\Component\Debug\DebugClassLoader;
-use Symfony\Component\Dotenv\Dotenv;
-require __DIR__ . '/../vendor/autoload.php';
 
 dump(Annotation::getPropertyAnnotations(new MyClass));
